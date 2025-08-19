5221f3e9cbc0864953b30bb0f8c5bbf4e8afa551
bia0016 <biaevangelistamoreira@gmail.com>
2025-08-19 13:24:00 -0300

    arquivos

--- listadecompra.c
+++ listadecompra.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 int main(){
     FILE * lis;
-    lis = fopen("lista.txt", "a");
-    if(lis == NULL){
+    lis = fopen("lista.txt", "r");
+        if(lis == NULL){
         printf("arquivo nao foi aberto\n");
         exit(1);
     }
     char produto[50];
     int qtd;
     float preco;
-    while(fscanf(lis, "%[^\n] %d %f", produto, qtd, preco) == EOF){
-        for(int i = 0; i < EOF; i++){
-            fprintf(lis, "%d) %s - %d x %.2f = %.2f", i, produto, qtd, preco, qtd * preco);
-        }
+    char str[1000];
+    
+    while(fscanf(lis, "%[^\n] %d %f", produto, qtd, preco) != EOF){
+        sprintf(str, "%d) %s - %d x %.2f = %.2f\n", i, produto, qtd, preco, qtd * preco);
     }
     printf("arquivo aberto com sucesso\n");
     fclose(lis);
+
+    FILE * out;
+    out = fopen("lista.txt", "a");
+        if(out == NULL){
+        printf("arquivo nao foi aberto\n");
+        exit(1);
+    }
+
+    fprintf(out, "%s", str);
+
     return 0;
 }
